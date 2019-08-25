f07dd2921bbe2e72ffe7d5d52d14c181cba3dc84
Diego Cid <diego.cid@outlook.com>
2019-08-24 20:22:27 -0400

    Configure heroku deployment

---
+++ RetrospectiveApp/config-env.ts
@@ -0,0 +1,23 @@
+import { writeFile } from 'fs';
+
+require('dotenv').config();
+
+const apiUrl = process.env.API_URL;
+const messagesUrl = process.env.MESSAGES_URL;
+const googleClientId = process.env.GOOGLE_CLIENT_ID;
+
+const targetPath = `./src/environments/environment.prod.ts`;
+const envConfigFile = `
+export const environment = {
+    production: true,
+    apiUrl: '${apiUrl}'},
+    messagesUrl: '${messagesUrl}',
+    googleClientId: '${googleClientId}'
+};
+`;
+
+writeFile(targetPath, envConfigFile, (err) => {
+  if (err) {
+       console.log(err);
+  }
+});
--- RetrospectiveApp/src/environments/environment.prod.ts
+++ RetrospectiveApp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: process.env.API_URL,
-  messagesUrl: process.env.MESSAGES_URL,
-  googleClientId: process.env.GOOGLE_CLIENT_ID
+  apiUrl: '',
+  messagesUrl: '',
+  googleClientId: ''
 };
